fcc16ce24561ca94710dd0eccde97796b3acb002
randychen <randychenis@gmail.com>
2020-06-03 00:19:42 +0800

    faster

--- src/main/java/com/summary/api/consumer/HeadlineScrapper.java
+++ src/main/java/com/summary/api/consumer/HeadlineScrapper.java
@@ -160,39 +160,39 @@ public class HeadlineScrapper {
             driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
             log.info("Loading url.. resting for 6 seconds to load");
             driver.get(url);
-            Thread.sleep(6000);
+            Thread.sleep(3000);
             driver.findElement(By.tagName("body")).click();
 //            driver.findElement(By.tagName("body")).sendKeys(Keys.PAGE_DOWN);
             WebElement ele = driver.findElement(By.className(content));
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ele);
             log.info("Scrolled down.. resting for 4 seconds to load");
-            Thread.sleep(4000);
+            Thread.sleep(3000);
             if (isFindByClass) {
                 result = driver.findElementByClassName(content).getText();
 
             } else {
                 result = driver.findElementById(content).getText();
             }
-            if (!filters.isEmpty()) {
-                List<WebElement> webElements = driver.findElementByClassName(content).findElements(By.xpath(".//*"));
-//                    List<WebElement> webElements =  driver.findElements(By.xpath("//*[@id=\"node-article-news-article-group-column-1\"]/div[2]/*"));
-                for (WebElement element : webElements) {
-                    if (isFilterByClass) {
-                        if (element.getAttribute("class") != null) {
-                            if (filters.stream().parallel().anyMatch(element.getAttribute("class")::contains)) {
-                                result = result.replaceFirst(element.getText(), "").trim();
-                            }
-                        }
-                    } else {
-                        if (filters.contains(element.getTagName())) {
-                            result = result.replaceFirst(element.getText(), "").trim();
-                        }
-                    }
-                    if(element.getTagName().equals("a")){
-                        result = result.replaceFirst(element.getText(), "").trim();
-                    }
-                }
-            }
+//            if (!filters.isEmpty()) {
+//                List<WebElement> webElements = driver.findElementByClassName(content).findElements(By.xpath(".//*"));
+////                    List<WebElement> webElements =  driver.findElements(By.xpath("//*[@id=\"node-article-news-article-group-column-1\"]/div[2]/*"));
+//                for (WebElement element : webElements) {
+//                    if (isFilterByClass) {
+//                        if (element.getAttribute("class") != null) {
+//                            if (filters.stream().parallel().anyMatch(element.getAttribute("class")::contains)) {
+//                                result = result.replaceFirst(element.getText(), "").trim();
+//                            }
+//                        }
+//                    } else {
+//                        if (filters.contains(element.getTagName())) {
+//                            result = result.replaceFirst(element.getText(), "").trim();
+//                        }
+//                    }
+//                    if(element.getTagName().equals("a")){
+//                        result = result.replaceFirst(element.getText(), "").trim();
+//                    }
+//                }
+//            }
             driver.close();
             return result.replaceAll("ADVERTISEMENT", "").trim();
 
